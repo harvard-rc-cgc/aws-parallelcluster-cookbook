37132511e6bb3ef5a3d8c339b1c6c7fc26be7eb2
Justin Riley <justin.t.riley@gmail.com>
2025-10-21 11:21:38 -0400

    update redhat8 packages list for rhel9

--- cookbooks/aws-parallelcluster-platform/resources/install_packages/install_packages_redhat8.rb
+++ cookbooks/aws-parallelcluster-platform/resources/install_packages/install_packages_redhat8.rb
@@ -23,13 +23,13 @@ def default_packages
   # environment-modules required by EFA, Intel MPI and ARM PL
   # Removed libssh2-devel from base_packages since is not shipped by RedHat 8 and in conflict with package libssh-0.9.6-3.el8.x86_64
   # iptables needed for IMDS setup
-  packages = %w(vim ksh tcsh zsh openssl-devel ncurses-devel pam-devel net-tools openmotif-devel
+  packages = %w(vim-minimal vim-filesystem vim-common vim-enhanced ksh tcsh zsh openssl-devel ncurses-devel pam-devel net-tools openmotif-devel
      libXmu-devel hwloc-devel libdb-devel tcl-devel automake autoconf libtool
      httpd boost-devel mlocate R atlas-devel
      blas-devel libffi-devel dkms libedit-devel jq
      libical-devel sendmail libxml2-devel libglvnd-devel
      libgcrypt-devel libevent-devel glibc-static bind-utils
-     iproute NetworkManager-config-routing-rules python3 python3-pip iptables libcurl-devel yum-plugin-versionlock
+     iproute NetworkManager-config-routing-rules python3 python3-pip iptables-nft iptables-libs libcurl-devel yum-plugin-versionlock
      coreutils moreutils curl environment-modules gcc gcc-c++ bzip2 dos2unix)
 
   if aws_region.start_with?("us-iso")
